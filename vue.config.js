1755c4daef18589d057c12993ebdbb4d0107d74d
lifiij <migoviprog@gmail.com>
2023-09-11 21:36:54 +0400

    update preview

--- vue.config.js
+++ vue.config.js
@@ -8,5 +8,5 @@ module.exports = defineConfig({
       },
     },
   },
-  publicPath: process.env.NODE_ENV === "production" ? "/nft-marketplace/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/NFTMarketplace/" : "/",
 });
